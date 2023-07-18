b3168157b8550c1b2e0718a1694f665943884aed
Severin Ibarluzea <seveibar@gmail.com>
2023-07-17 18:37:17 -0700

    testing w/ refs

--- src/index.ts
+++ src/index.ts
@@ -10,6 +10,13 @@ import { useCallback, useEffect, useRef, useState } from "react"
 
 type Point = { x: number; y: number }
 
+type MouseState = {
+  md: boolean
+  m0: Point
+  m1: Point
+  mlastrel: Point
+}
+
 interface Props {
   canvasElm?: HTMLElement
   transform?: Matrix
@@ -19,11 +26,22 @@ interface Props {
 
 export const useMouseMatrixTransform = (props: Props = {}) => {
   const extRef = useRef<any>(null)
+  const { current: ms } = useRef<MouseState>({
+    m0: { x: 0, y: 0 },
+    m1: { x: 0, y: 0 },
+    mlastrel: { x: 0, y: 0 },
+    md: false,
+  })
+  const tref = useRef<{ matrix: Matrix | null }>({ matrix: null })
   const canvasElm = props.canvasElm ?? extRef.current
   const [internalTransform, setInternalTransform] = useState<Matrix>(
     props.initialTransform ?? identity()
   )
 
+  // The references inside this function to setInternalTransform might be lost
+  // when the component's second ref is found, we could try to make it so this
+  // useEffect dance doesn't even happen if the div doesn't actually change
+  // or something
   const setTransform = (newTransform: Matrix) => {
     if (props.onSetTransform) {
       props.onSetTransform(newTransform)
@@ -32,19 +50,16 @@ export const useMouseMatrixTransform = (props: Props = {}) => {
     }
   }
 
-  const transform = props.transform ?? internalTransform
+  const transform = tref.current.matrix ?? props.transform ?? internalTransform
 
   useEffect(() => {
     // redefine canvasElm, it can sometimes not be defined in
     // render but be defined in effects due to SSR
     const canvasElm = props.canvasElm ?? extRef.current
     if (!canvasElm) return
-    let init_tf = props.transform ?? internalTransform
-
-    let m0: Point = { x: 0, y: 0 },
-      m1: Point = { x: 0, y: 0 },
-      md = false,
-      mlastrel: Point = { x: 0, y: 0 }
+    if (!tref.current.matrix) {
+      tref.current.matrix = props.transform ?? internalTransform
+    }
 
     const getMousePos = (e: MouseEvent) => {
       // Using the offset for the elemenet messes things up when the element
@@ -62,46 +77,58 @@ export const useMouseMatrixTransform = (props: Props = {}) => {
     }
 
     function handleMouseDown(e: MouseEvent) {
-      m0 = getMousePos(e)
-      md = true
+      ms.m0 = getMousePos(e)
+      ms.md = true
       e.preventDefault()
     }
     function handleMouseUp(e: MouseEvent) {
-      m1 = getMousePos(e)
+      ms.m1 = getMousePos(e)
+      const { m0, m1 } = ms
 
-      const new_tf = compose(translate(m1.x - m0.x, m1.y - m0.y), init_tf)
+      const new_tf = compose(
+        translate(m1.x - m0.x, m1.y - m0.y),
+        tref.current.matrix!
+      )
       setTransform(new_tf)
-      init_tf = new_tf
+      tref.current.matrix = new_tf
 
-      md = false
+      ms.md = false
     }
     function handleMouseMove(e: MouseEvent) {
-      mlastrel = getMousePos(e)
+      const { m0, m1, md } = ms
+      ms.mlastrel = getMousePos(e)
       if (!md) return
-      m1 = getMousePos(e)
+      ms.m1 = getMousePos(e)
 
-      setTransform(compose(translate(m1.x - m0.x, m1.y - m0.y), init_tf))
+      setTransform(
+        compose(translate(m1.x - m0.x, m1.y - m0.y), tref.current.matrix!)
+      )
     }
     function handleMouseWheel(e: WheelEvent) {
+      console.log("wheel")
       const center = getMousePos(e)
       const new_tf = compose(
         translate(center.x, center.y),
         scale(1 - e.deltaY / 1000, 1 - e.deltaY / 1000),
         translate(-center.x, -center.y),
-        init_tf
+        tref.current.matrix!
       )
       setTransform(new_tf)
-      init_tf = new_tf
+      tref.current.matrix = new_tf
       e.preventDefault()
     }
     function handleMouseOut(e: MouseEvent) {
-      if (!md) return
-      md = false
-      m1 = getMousePos(e)
+      if (!ms.md) return
+      ms.md = false
+      ms.m1 = getMousePos(e)
 
-      const new_tf = compose(translate(m1.x - m0.x, m1.y - m0.y), init_tf)
+      const { m0, m1 } = ms
+      const new_tf = compose(
+        translate(m1.x - m0.x, m1.y - m0.y),
+        tref.current.matrix!
+      )
       setTransform(new_tf)
-      init_tf = new_tf
+      tref.current.matrix = new_tf
     }
 
     canvasElm.addEventListener("mousedown", handleMouseDown)
@@ -120,8 +147,11 @@ export const useMouseMatrixTransform = (props: Props = {}) => {
   }, [canvasElm])
 
   const applyTransformToPoint = useCallback(
-    (obj: Point | [number, number]) => applyToPoint(transform, obj),
-    [transform]
+    (obj: Point | [number, number]) => {
+      console.log(tref.current.matrix?.e)
+      return applyToPoint(tref.current.matrix ?? transform, obj)
+    },
+    [transform, tref.current.matrix]
   )
 
   return {
